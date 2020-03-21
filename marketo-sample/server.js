ff0a8f33ebd40b0b9e7cf519ce18a0a1289f537e
Tse Chen <Tse.Chen@arm.com>
2020-03-21 00:03:01 -0700

    update

---
+++ marketo-sample/server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const path = require('path')
+const app = express();
+const port = 3000;
+
+app.get('/', (req, res) => {
+    res.sendFile('./src/index.html', { root: __dirname });
+});
+
+app.use('/static', express.static(path.join(__dirname, 'static')))
+
+// app.get('/about', (req, res) => {
+//     res.sendFile('./landing-page/about.html', { root: __dirname });
+// });
+
+// app.get('/contact', (req, res) => {
+//     res.sendFile('./landing-page/contact.html', { root: __dirname });
+// });
+
+app.listen(port, () => console.log(`listening on port ${port}!`))
